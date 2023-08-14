823564af7831e8ffc38dcc6c1ef065d1802bf845
Sergey Toporets <yateperna@yandex.ru>
2023-08-14 12:30:24 +0300

    feat: Написал первую версию ELK

---
+++ docker/docker-elk/main.py
@@ -0,0 +1,50 @@
+"""Генерирует тестовые логи и записывает в файл."""
+
+import datetime
+import typing
+import json
+
+import uvicorn # type: ignore
+from loguru import logger # type: ignore
+from fastapi import FastAPI # type: ignore
+
+
+app = FastAPI()
+
+logger.add(
+    "/var/log/file.log",
+    format="{message}",
+    rotation="500 MB",
+)
+
+
+def generate_log_message() -> dict[str, typing.Any]:
+    """
+    Генерирует сообщения журнала.
+
+    Returns:
+        dict: Словарь лога.
+    """
+    timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat(
+        timespec="milliseconds", sep=" ",
+    )
+    message = "This is a sample log message"
+    log_message = {"@timestamp": timestamp, "message": message}
+    logger.info(log_message)
+
+    return log_message
+
+
+@app.get("/")
+async def write_log_file() -> dict[str, typing.Any]:
+    """
+    Записывает файл лога в каталог.
+
+    Returns:
+        dict: Сгенерированное сообщение лога.
+    """
+    return generate_log_message()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

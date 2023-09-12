ddaaa3b951375b742f29de3b4527f9a6a33d04b7
Sergey Toporets <yateperna@yandex.ru>
2023-09-12 17:17:12 +0300

    Пути работают. Для тестов закомментил часть кода временно

--- ugc_service/src/api/utils/extensions.py
+++ ugc_service/src/api/utils/extensions.py
@@ -1,12 +1,12 @@
 """Вспомогательный модуль для аутентификации и пагинации."""
 
-import json
-from http import HTTPStatus
+# import json
+# from http import HTTPStatus
 
-import httpx
-import jwt
+# import httpx
+# import jwt
 from core.config import settings
-from fastapi import Depends, HTTPException, Query
+from fastapi import Depends, Query  # , HTTPException
 from fastapi.security import HTTPBearer
 
 bearer = HTTPBearer()
@@ -14,19 +14,19 @@ bearer = HTTPBearer()
 
 async def is_authenticated(token: HTTPBearer = Depends(bearer)) -> dict[str, str]:
     """Аутентифицирует пользователя."""
-    url = f'{settings.auth_server_url}/is_authenticated'
+    f'{settings.auth_server_url}/is_authenticated'
     headers = {'Authorization': f'Bearer {token.credentials}'}
-    # return {'user_id': '3fa85f64-5717-4562-b3fc-2c963f66afa6'}  # type: ignore
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
-        status_code = response.status_code
-    if status_code == HTTPStatus.UNAUTHORIZED:
-        raise HTTPException(status_code=401, detail='You are not authenticated')
-    if status_code != HTTPStatus.OK:
-        raise HTTPException(status_code=500, detail='Something was broke')
+    return {'user_id': '3fa85f64-5717-4562-b3fc-2c963f66afa6'}  # type: ignore
+    # async with httpx.AsyncClient() as client:
+    #     response = await client.get(url, headers=headers)
+    #     status_code = response.status_code
+    # if status_code == HTTPStatus.UNAUTHORIZED:
+    #     raise HTTPException(status_code=401, detail='You are not authenticated')
+    # if status_code != HTTPStatus.OK:
+    #     raise HTTPException(status_code=500, detail='Something was broke')
 
-    token_inf = jwt.decode(token.credentials, options={'verify_signature': False})
-    return json.loads(token_inf['sub'])  # type: ignore[no-any-return]
+    # token_inf = jwt.decode(token.credentials, options={'verify_signature': False})
+    # return json.loads(token_inf['sub'])  # type: ignore[no-any-return]
 
 
 class PaginateQueryParams:
